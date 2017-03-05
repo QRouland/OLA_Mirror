5f5101061567695f8739170d78144a91bf7b66a1
matthieupenchenat81 <matthieu-p93@live.fr>
2017-03-05 10:55:38 +0100

    TG-117 : remove bootstrap css framework and replace by angular-material and create login page

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on 2017-01-20
+// Generated on 2017-02-26
 
 module.exports = function(config) {
   'use strict';
@@ -20,10 +20,13 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-material/angular-material.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower

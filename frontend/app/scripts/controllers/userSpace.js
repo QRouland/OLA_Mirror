a76d7a9604fffc6bc68e0e35310e60322a81f575
mpenchenat <matthieu.penchenat@protonmail.com>
2017-03-14 08:21:12 +0100

    TG-119 : update user space

--- frontend/app/scripts/controllers/userSpace.js
+++ frontend/app/scripts/controllers/userSpace.js
@@ -5,12 +5,61 @@
         .controller('UserSpaceCtrl', ['$scope', '$state',
             function ($scope, $state) {
 
+                $scope.toggleAccordion = toggleAccordion;
+                $scope.isOpenAccordion = isOpenAccordion;
+                $scope.logout = logout;
+                $scope.exportBooklet = exportBooklet;
+
                 init();
 
                 // ---------------------------------------------------------------
 
                 function init() {
-                   
+
+                    $scope.periods = [
+                        {
+                            number : 2,
+                            company: {
+                                icon : 'add',
+                                comment: ''
+                            }, 
+                            university: {
+                                icon : 'add',
+                                comment: ''
+                            }
+                        },{
+                            number : 1,
+                            company: {
+                                icon : 'add',
+                                comment: ''
+                            }, 
+                            university: {
+                                icon : 'add',
+                                comment: ''
+                            }
+                        }
+                    ];
+                }
+
+                function toggleAccordion(value, isCompany, index) {
+                    var newValue = (value === 'add') ? 'remove': 'add';
+                    if(isCompany === 'true') {
+                        $scope.periods[index].company.icon = newValue;
+                    } else {
+                        $scope.periods[index].university.icon = newValue;
+                    }
+                }
+
+                function isOpenAccordion(value) {
+                    return (value === 'remove');
+                }
+
+                function logout() {
+                     $state.go('login');
+                }
+
+                function exportBooklet() {
+                    console.log('export booklet .. TODO ');
                 }
 
                 // ---------------------------------------------------------------

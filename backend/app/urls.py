7a9f64c52923d33a34214d8db63cf9a3531512c7
Clément ARNAUDEAU <clement.arnaudeau@yahoo.fr>
2017-03-28 17:57:51 +0200

    TG-124 : UserGroupsAPI

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,8 @@
 from app.api.GroupAPI import GroupAPI
+from app.api.LivretAPI import LivretAPI
 from app.api.LoginAPI import LoginAPI
 from app.api.UserAPI import UserAPI
-from app.api.UserInfoAPI import UserInfoAPI
+from app.api.UserInfoAPI import UserInfoAPI, UserGroupsAPI
 from app.api.exampleapi import SomeApi
 from app.core import api
 
@@ -9,6 +10,8 @@ from app.core import api
 api.add_resource(SomeApi, '/api/someapi', '/api/someapi/<int:id>')
 api.add_resource(LoginAPI, '/api/login')
 api.add_resource(UserInfoAPI, '/api/userInfo')
+api.add_resource(UserGroupsAPI, '/api/userGroups')
 api.add_resource(UserAPI, '/api/user', '/api/user/byuid/<int:uid>', '/api/user/byemail/<string:email>',
                  '/api/user/byhash/<string:hashcode>')
 api.add_resource(GroupAPI, '/api/group', '/api/group/bygid/<int:gid>', '/api/group/byname/<string:name>')
+api.add_resource(LivretAPI, '/api/livret', '/api/livret/byuid/<int:uid>')
